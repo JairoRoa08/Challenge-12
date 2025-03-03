c1a9f8a70eae0fe8d86117404fd0320967a0e150
Jairo Roa <Jroa@usf.edu>
2025-03-02 22:48:38 -0500

    Task 2 - Updated Metric Cards via Array Conversion

--- cc_12.js
+++ cc_12.js
@@ -10,3 +10,12 @@ metricCard.innerHTML= "<h3>Revenue</h3><p>$0</p>";
 } else {
     console.error("Dashboard container not found.");
 }
+
+// Task 2: Updating Dashboard Metrics – Working with NodeLists and Arrays
+const metricCards = document.querySelectorAll (".metric-card");
+const metricCardsArray = Array.from(metricCards);
+metricCardsArray.forEach (card => {
+    card.innerHTML += "-Updated";
+    card.computedStyleMap.backgroundColor = "#f0f0f0";
+});
+
